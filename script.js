5e4ed5021b71dd161df6e55885f87391c64cf53a
gargsneh <gargsneh58@gmail.com>
2024-01-15 22:17:40 +0530

    final

---
+++ script.js
@@ -0,0 +1,48 @@
+const productList = document.getElementById("product-list");
+const searchInput = document.getElementById("searchinput");
+
+const searchBtn = document.getElementById("search-btn");
+const categoryBtns = document.querySelectorAll(".category-btn");
+
+
+function filterProduct() {
+    const searchValue = searchInput.value.toLowerCase();
+    const productItems = document.querySelectorAll(".product-items");
+    const activeCategory = document.querySelector(".category-btn.active").dataset.category;
+
+    productItems.forEach(item=> {
+        const title = item.querySelector("h3").innerText.toLowerCase();
+        const category = item.dataset.category;
+
+
+        if ((title.includes(searchValue) || searchValue == "" ) && (category === activeCategory|| activeCategory=== "all"))
+        {
+            item.style.display = "block";
+
+        }
+        else {
+            item.style.display = "none";
+        }
+
+    })
+
+}
+function setCategory(e){
+
+
+     categoryBtns.forEach(btn=>btn.classList.remove("active"));
+
+        
+        e.target.classList.add("active");
+        filterProduct();
+     }
+
+searchBtn.addEventListener("click", filterProduct);
+searchInput.addEventListener("keyup", filterProduct);
+categoryBtns.forEach(btn => {
+    btn.addEventListener("click", setCategory);
+
+})
+filterProduct();
+
+
